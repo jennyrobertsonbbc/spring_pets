578264c5593682c0907a006210a55b7dfaa1103e
Jenny Robertson <jenny.robertson@bbc.co.uk>
2016-10-12 16:32:16 +0100

    Added hearts, etc to front end

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -9,11 +9,41 @@ $(document).ready(function(){
      $.get("http://localhost:8080/pets", function(data) {
           $.each(data, function(i, pet) {
 
-                        $("body").append(
-                            "<tr><td>" + pet.name + "</td>" +
-                            "<td>" + pet.age + "</td>" +
-                            "<td>" + pet.hunger + "</td></tr>");
+                        $("#container").append(
+            '<div class="showPet">'+
+
+           '<div class="petColumnLeft">'+
+                '<img class="petImage" src=""/>'+
+            '</div>'+
+
+           '<div class="petColumnMiddle">'+
+                '<h2 class="petName">' + pet.name + '</h2>'+
+                '<span class="petAge">' + pet.age + ' years old</span>'+
+            '</div>'+
+
+            '<div class="petColumnRight">'+
+                '<span class="petOwner">Owned by ' + pet.ownerId + '</span><br>'+
+                // '<span class="petHunger">Hunger: ' + pet.hunger + '</span><br>'+
+                '<span class="petId">Id: ' + pet.petId +'</span>'+
+
+            '</div>'+
+
+            '<div class="petRowBottom">'+
+                '<button>Feed</button>'+
+
+                '<div class="petHungerSlider">'+
+                '<div class="petHungerSliderHearts"></div>'+
+                    
+                    '<div class="petHungerSliderInner" style="width:'+ pet.hunger + '%"></div>'+
+
+                '</div>'+
+
+           ' </div>'+
+
+        '</div><!-- show pet -->');
                     });
 
                 });
 });
+
+
